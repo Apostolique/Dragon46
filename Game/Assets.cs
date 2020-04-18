0fc7f7b843ad84f6afff793ce17106c8159f435e
Jean-David Moisan <vynecompiler@gmail.com>
2020-04-18 12:07:56 -0400

    Add infinite background and sort layers

--- Game/Assets.cs
+++ Game/Assets.cs
@@ -4,9 +4,13 @@ using Microsoft.Xna.Framework.Graphics;
 namespace GameProject {
     public static class Assets {
         public static void Setup(ContentManager content) {
+            Infinite = content.Load<Effect>("infinite");
+            Sky = content.Load<Texture2D>("sky");
             Bush = content.Load<Texture2D>("bush");
         }
 
+        public static Effect Infinite;
+        public static Texture2D Sky;
         public static Texture2D Bush;
     }
 }
--- Game/Backgrounds.cs
+++ Game/Backgrounds.cs
@@ -7,12 +7,32 @@ namespace GameProject {
     public class Backgrounds {
         public Backgrounds(GraphicsDevice g) {
             _s = new SpriteBatch(g);
-            Quadtree<BackgroundObjects>.Add(new BackgroundObjects(0, 0, -2, Assets.Bush));
-            Quadtree<BackgroundObjects>.Add(new BackgroundObjects(0, 0, -1, Assets.Bush));
-            Quadtree<BackgroundObjects>.Add(new BackgroundObjects(0, 0, 0, Assets.Bush));
+            Quadtree<BackgroundObjects>.Add(new BackgroundObjects(0, 0, -4, Assets.Bush));
+            Quadtree<BackgroundObjects>.Add(new BackgroundObjects(0, 0, -3, Assets.Bush));
+            Quadtree<BackgroundObjects>.Add(new BackgroundObjects(400, 0, -2, Assets.Bush));
+            Quadtree<BackgroundObjects>.Add(new BackgroundObjects(-300, 0, -1, Assets.Bush));
         }
 
         public void Draw() {
+            Rectangle view = _s.GraphicsDevice.Viewport.Bounds;
+            Vector2 size = new Vector2(Assets.Sky.Width, Assets.Sky.Height);
+            var scale = 10;
+            var posOffset = new Vector2(0, 0);
+
+            Matrix m =
+                Matrix.CreateScale(scale) *
+                Matrix.CreateScale(size.X, size.Y, 1) *
+                Matrix.CreateTranslation(posOffset.X, posOffset.Y, 1) *
+                CameraWrapper.Camera.View(-10) *
+                Matrix.CreateScale(1f / size.X, 1f / size.Y, 1);
+
+            Assets.Infinite.Parameters["ScrollMatrix"].SetValue(Matrix.Invert(m));
+            Assets.Infinite.Parameters["ViewportSize"].SetValue(new Vector2(view.Width, view.Height));
+
+            _s.Begin(samplerState: SamplerState.LinearWrap, effect: Assets.Infinite);
+            _s.Draw(Assets.Sky, new Vector2(0, 0), view, Color.White);
+            _s.End();
+
             var byLayer = Quadtree<BackgroundObjects>.Query(Quadtree<BackgroundObjects>.Bounds).GroupBy(
                 backgroundObject => backgroundObject.Z,
                 backgroundObject => backgroundObject,
@@ -20,7 +40,7 @@ namespace GameProject {
                     Z = z,
                     BackgroundObjects = backgroundObjects
                 }
-            );
+            ).OrderBy(l => l.Z);
 
             foreach (var l in byLayer) {
                 _s.Begin(transformMatrix: CameraWrapper.Camera.View(l.Z));
