efa1414863f22094224b6fdcd04e009043a61a59
Jean-David Moisan <vynecompiler@gmail.com>
2020-04-18 11:11:08 -0400

    Add layered backgrounds

--- Game/BackgroundObject.cs
+++ Game/BackgroundObject.cs
@@ -5,16 +5,27 @@ using MonoGame.Extended;
 
 namespace GameProject {
     public class BackgroundObjects : IAABB {
-        public BackgroundObjects(int x, int y, int width, int height) {
+        public BackgroundObjects(int x, int y, float z, int width, int height, Color c) {
             _aabb = new Rectangle(x, y, width, height);
+            Z = z;
+            Color = c;
         }
 
         public Rectangle AABB => _aabb;
         public float Angle => 0;
         public Vector2 Origin => Vector2.Zero;
+        public Color Color {
+            get;
+            set;
+        }
+
+        public float Z {
+            get;
+            set;
+        } = 0;
 
         public void Draw(SpriteBatch s) {
-            s.FillRectangle(AABB, Color.White);
+            s.FillRectangle(AABB, Color);
         }
 
         private Rectangle _aabb;
--- Game/Backgrounds.cs
+++ Game/Backgrounds.cs
@@ -1,19 +1,34 @@
+using System.Linq;
 using Dcrew.MonoGame._2D_Spatial_Partition;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using MonoGame.Extended;
 
 namespace GameProject {
     public class Backgrounds {
         public Backgrounds(GraphicsDevice g) {
             _s = new SpriteBatch(g);
-            Quadtree<BackgroundObjects>.Add(new BackgroundObjects(0, 0, 100, 200));
+            Quadtree<BackgroundObjects>.Add(new BackgroundObjects(0, 0, -2, 100, 200, Color.Red));
+            Quadtree<BackgroundObjects>.Add(new BackgroundObjects(0, 0, -1, 100, 200, Color.Beige));
+            Quadtree<BackgroundObjects>.Add(new BackgroundObjects(0, 0, 0, 100, 200, Color.Blue));
         }
 
         public void Draw() {
-            _s.Begin(transformMatrix: CameraWrapper.Camera.View(0));
-            foreach (var e in Quadtree<BackgroundObjects>.Query(Quadtree<BackgroundObjects>.Bounds))
-                e.Draw(_s);
-            _s.End();
+            var byLayer = Quadtree<BackgroundObjects>.Query(Quadtree<BackgroundObjects>.Bounds).GroupBy(
+                backgroundObject => backgroundObject.Z,
+                backgroundObject => backgroundObject,
+                (z, backgroundObjects) => new {
+                    Z = z,
+                    BackgroundObjects = backgroundObjects
+                }
+            );
+
+            foreach (var l in byLayer) {
+                _s.Begin(transformMatrix: CameraWrapper.Camera.View(l.Z));
+                foreach (var e in l.BackgroundObjects)
+                    e.Draw(_s);
+                _s.End();
+            }
         }
 
         SpriteBatch _s;
