dfe4dd8a27bbe0836c3845fa618d0d44dd208c74
pandepic <39251913+pandepic@users.noreply.github.com>
2020-04-20 16:37:43 +1000

    added instructions

--- Game/HowToPlayState.cs
+++ Game/HowToPlayState.cs
@@ -27,22 +27,32 @@ namespace GameProject
             helpLabel2.CenterX(graphics);
             helpLabel2.Position.Y = 305;
 
-            var helpLabel3 = new UILabel("If any of your party die you will lose the game.", 42, 5);
+            var helpLabel3 = new UILabel("Healing Light: big burst heal, Regeneration: heal over time,", 42, 5);
             helpLabel3.CenterX(graphics);
             helpLabel3.Position.Y = 360;
+
+            var helpLabel4 = new UILabel("Barrier: resist damage, Silence: cancel enemy spell, Distract: delay enemy spell.", 42, 5);
+            helpLabel4.CenterX(graphics);
+            helpLabel4.Position.Y = 415;
+
+            var helpLabel5 = new UILabel("If any of your party die you will lose the game.", 42, 5);
+            helpLabel5.CenterX(graphics);
+            helpLabel5.Position.Y = 470;
 
             var startButton = new UIButton("Start Game", 42, 5)
             {
                 OnClick = () => { _newState = GameStateType.InGame; }
             };
             startButton.CenterX(graphics);
-            startButton.Position.Y = 450;
+            startButton.Position.Y = 550;
 
             UIHelper.AddButton(startButton);
             UIHelper.AddLabel(titleLabel);
             UIHelper.AddLabel(helpLabel1);
             UIHelper.AddLabel(helpLabel2);
             UIHelper.AddLabel(helpLabel3);
+            UIHelper.AddLabel(helpLabel4);
+            UIHelper.AddLabel(helpLabel5);
         }
 
         public override GameStateType Update(GameTime gameTime)
